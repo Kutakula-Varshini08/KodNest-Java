1e229af2d6d06a0c88c5378c91490e3a37df44a8
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-04 12:30:38 +0530

    Program of if else if ladder

---
+++ track/JavaPracticeTrack/Number.java
@@ -0,0 +1,15 @@
+
+class Number {
+
+    public static void main(String[] args) {
+        int Number = -4;
+        if (Number > 0) {
+            System.out.println("Positive");
+        } else if (Number == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Negative");
+        }
+
+    }
+}
