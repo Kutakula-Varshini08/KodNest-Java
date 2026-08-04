b95bd050c16169b2e69de69596a5200c032d1177
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-04 19:09:52 +0530

    conditional problems

--- track/M02/M02/T02/ConditionalProblem.java
+++ track/M02/M02/T02/ConditionalProblem.java
@@ -1,11 +1,10 @@
 
-class ConditionalPrograms {
+class ConditionalProblem {
 
     public static void main(String[] args) {
         int number = -7;
         int firstScore = 18;
         int secondScore = 25;
-
         if (number > 0) {
             System.out.println("Number type: Positive");
         } else if (number < 0) {
@@ -13,13 +12,11 @@ class ConditionalPrograms {
         } else {
             System.out.println("Number type: Zero");
         }
-
         if (number % 2 == 0) {
             System.out.println("Parity: Even");
         } else {
             System.out.println("Parity: Odd");
         }
-
         if (firstScore >= secondScore) {
             System.out.println("Larger score: " + firstScore);
         } else {
