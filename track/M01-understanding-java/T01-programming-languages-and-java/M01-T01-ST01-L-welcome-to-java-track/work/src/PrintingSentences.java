c44ef0abab0a2bb35172255fbeb81a6d19671e40
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-04 20:52:26 +0530

    Display the placement goal

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/PrintingSentences.java
@@ -0,0 +1,10 @@
+
+class PrintingSentences {
+
+    public static void main(String[] args) {
+        //Display the student's learning track
+        System.out.println("Track: Java Development");
+        //Display the student's daily practice
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
