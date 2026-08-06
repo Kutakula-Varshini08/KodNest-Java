f14b99633ff61eabdcdd4cf1507745cbbc9c840c
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-06 12:49:06 +0530

    adding break statement to nested loop

---
+++ track/JavaPracticeTrack/LoopWithBreak.java
@@ -0,0 +1,15 @@
+
+class LoopWithBreak {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 2; j++) {
+                if (j == 2) {
+                    break;
+                }
+                System.out.println("i: " + i + " j: " + j
+                );
+            }
+        }
+    }
+}
