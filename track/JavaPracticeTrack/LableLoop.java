48c0b6d6fc5eddcb43670cdf1b2f8c406d80c6f4
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-06 13:01:21 +0530

    Lable Loop

---
+++ track/JavaPracticeTrack/LableLoop.java
@@ -0,0 +1,17 @@
+
+class LableLoop {
+
+    public static void main(String[] args) {
+        Outer:
+        for (int i = 1; i <= 4; i++) {
+            Inner:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2) {
+                    break Outer;
+                }
+                System.out.println("i: " + i + " j: " + j
+                );
+            }
+        }
+    }
+}
