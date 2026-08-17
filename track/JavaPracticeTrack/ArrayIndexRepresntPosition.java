fd0202eadfc4ab48c0d31b6fa7727ab8069b4862
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-17 12:19:02 +0530

    Array problem

---
+++ track/JavaPracticeTrack/ArrayIndexRepresntPosition.java
@@ -0,0 +1,13 @@
+class ArrayIndexRepresntPosition{
+    public static void main(String[] args){
+        int a[] = {23,67,8,5,66,98,76};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+        System.out.println(a[5]);
+        System.out.println(a[6]);
+
+    }
+}
