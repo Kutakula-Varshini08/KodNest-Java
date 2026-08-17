552cef3ffc238066fe977dd5b9374c311b0ed197
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-17 12:46:09 +0530

    Array updation of the index positions

---
+++ track/JavaPracticeTrack/ArrayWithUserInput.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class ArrayWithUserInput {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter array elements: ");
+        int a[] = new int[5];
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = s.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
