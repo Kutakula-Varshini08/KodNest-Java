e007ad1366da3e3bc60b9387a29e20e7d42fc9fe
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-17 12:09:58 +0530

    Creating an Array

---
+++ track/JavaPracticeTrack/ArrayCreating.java
@@ -0,0 +1,22 @@
+
+class ArrayCreating {
+
+    public static void main(String[] args) {
+        int a[] = new int[7];
+        a[0] = 12;
+        a[1] = 88;
+        a[2] = 77;
+        a[3] = 78;
+        a[4] = 23;
+        a[5] = 25;
+        a[6] = 92;
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+        System.out.println(a[5]);
+        System.out.println(a[6]);
+
+    }
+}
