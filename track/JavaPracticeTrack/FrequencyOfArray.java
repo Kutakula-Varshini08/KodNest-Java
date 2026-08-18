86b94303322634d26fd545723bbdbfb53b94f63c
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-18 12:34:49 +0530

    Frequency of array

---
+++ track/JavaPracticeTrack/FrequencyOfArray.java
@@ -0,0 +1,23 @@
+
+class FrequencyOfArray {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 28, 34, 59, 10, 10};
+        Freq f = new Freq();
+        f.count(arr, 10);
+    }
+}
+
+class Freq {
+
+    int count = 0;
+
+    void count(int arr[], int key) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] == key) {
+                count++;
+            }
+        }
+        System.out.println(key + " : " + count);
+    }
+}
