4140870ba2838272f1ad3da57fb5927d50b89472
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-08-19 13:14:55 +0530

    2D and 3D Array

--- track/JavaPracticeTrack/ThreeDimensionalArray.java
+++ track/JavaPracticeTrack/ThreeDimensionalArray.java
@@ -1,21 +1,26 @@
 
 import java.util.Scanner;
 
-class TwoDimensionalArray {
+class ThreeDimensionalArray {
 
     public static void main(String[] args) {
-        int a[][] = new int[3][5];
+        int a[][][] = new int[3][3][5];
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter array elements: ");
         for (int i = 0; i <= a.length - 1; i++) {
             for (int j = 0; j <= a[i].length - 1; j++) {
-                a[i][j] = scan.nextInt();
+                for (int k = 0; k <= a[i][j].length - 1; k++) {
+                    a[i][j][k] = scan.nextInt();
+                }
             }
         }
         System.out.println("Array elements are: ");
         for (int i = 0; i <= a.length - 1; i++) {
             for (int j = 0; j <= a[i].length - 1; j++) {
-                System.out.print(a[i][j] + " ");
+                for (int k = 0; k <= a[i][j].length - 1; k++) {
+                    System.out.print(a[i][j][k] + " ");
+                }
+                System.out.println();
             }
             System.out.println();
         }
