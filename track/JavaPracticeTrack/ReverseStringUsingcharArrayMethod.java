c051231c7fd5eb49b947c3994b46b1810d039ea2
Tanusha12 <87524506+Tanusha12@users.noreply.github.com>
2026-09-10 17:42:30 +0530

    StringBuilder Program and reverse of String

---
+++ track/JavaPracticeTrack/ReverseStringUsingcharArrayMethod.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+class ReverseStringUsingcharArrayMethod {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str = s.nextLine();
+        char arr[] = str.toCharArray();
+        char newArr[] = new char[arr.length];
+        int j = newArr.length - 1;
+        for (int i = 0; i < arr.length; i++) {
+            newArr[j] = arr[i];
+            j--;
+        }
+        String revStr = new String(newArr);
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + revStr);
+    }
+}
